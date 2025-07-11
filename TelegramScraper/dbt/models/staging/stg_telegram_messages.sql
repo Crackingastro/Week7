a6ad0dd52b5813fbadf5656634821b01c83c6a5d
Addisu <addisum443@gmail.com>
2025-07-12 00:56:19 +0300

    feat:add staging folder for source-level dbt models

---
+++ TelegramScraper/dbt/models/staging/stg_telegram_messages.sql
@@ -0,0 +1,30 @@
+{{
+  config(
+    materialized = 'view',
+    unique_key = 'message_id'
+  )
+}}
+
+WITH combined_messages AS (
+  SELECT id, text, date, views, _channel, _loaded_at
+  FROM {{ source('raw', 'telegram_messages_chemed123') }}
+  
+  UNION ALL
+  
+  SELECT id, text, date, views, _channel, _loaded_at
+  FROM {{ source('raw', 'telegram_messages_lobelia4cosmetics') }}
+  
+  UNION ALL
+  
+  SELECT id, text, date, views, _channel, _loaded_at
+  FROM {{ source('raw', 'telegram_messages_tikvahpharma') }}
+)
+
+SELECT
+  id::INTEGER AS message_id,
+  text AS message_text,
+  date::TIMESTAMP AS message_date,
+  _channel AS channel_name,
+  views::INTEGER AS view_count,
+  _loaded_at::TIMESTAMP AS loaded_at
+FROM combined_messages
